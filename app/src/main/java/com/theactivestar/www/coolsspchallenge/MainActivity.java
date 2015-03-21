35cbb40161f986c4ee5bbd27d7695143e9c55379
mukund1703 <chauhan.mukund.m@gmail.com>
2015-03-21 15:25:46 +0530

    Initial commit

---
+++ app/src/main/java/com/theactivestar/www/coolsspchallenge/MainActivity.java
@@ -0,0 +1,57 @@
+package com.theactivestar.www.coolsspchallenge;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Handler;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout li = (RelativeLayout) findViewById(R.id.back);
+
+        Handler mh = new Handler();
+        mh.postDelayed(mU, 2000);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Runnable mU = new Runnable() {
+        public void run() {
+
+            Intent i = new Intent(MainActivity.this, menu.class);
+            startActivity(i);
+            finish();
+        }
+    };
+}
---
+++ app/src/main/java/com/theactivestar/www/coolsspchallenge/menu.java
@@ -0,0 +1,28 @@
+package com.theactivestar.www.coolsspchallenge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Mukund on 20/03/2015.
+ */
+public class menu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
